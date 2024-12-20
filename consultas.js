c7edf0990a63c37649c2355b8b7c038aae78cd8d
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-12-20 00:33:03 -0500

    first commit

---
+++ consultas.js
@@ -0,0 +1,57 @@
+const { Pool } = require("pg");
+const pool = new Pool({
+ host: "localhost",
+ user: "postgres",
+ password: "Vanadi123",
+ database: "likeme",
+ allowExitOnIdle: true,
+});
+ 
+
+// metodo post
+
+const agregarPosts = async (titulo, img, descripcion, likes ) => {
+    const consulta = "INSERT INTO posts values (DEFAULT, $1, $2, $3, $4)";
+    const values = [titulo, img, descripcion, likes];
+    const result = await pool.query(consulta, values);
+    console.log("Post agregado");
+   };
+
+/* agregarPosts(); */
+
+
+// metodo get 
+
+const obtenerPosts = async () => {
+    const { rows } = await pool.query("SELECT * FROM posts");
+    console.log(rows);
+    return rows;
+   };
+ /*   obtenerPosts(); */
+
+// metodo put 
+
+
+const modificarPosts = async (id, titulo) => {
+    const consulta = "UPDATE posts SET titulo= $2 WHERE id = $1";
+    const values = [id, titulo];
+    const result = await pool.query(consulta, values);
+    const { rowCount } = await pool.query(consulta, values);
+    if (rowCount === 0) {
+      throw { code: 404, message: "No se consiguió ningún posts con este id" };
+    }
+  };
+
+
+
+  // metodo delete
+
+
+  const eliminarPosts = async (id) => {
+    const consulta = "DELETE FROM posts WHERE id = $1";
+    const values = [id];
+    const result = await pool.query(consulta, values);
+   };
+
+
+   module.exports = {agregarPosts, obtenerPosts, modificarPosts, eliminarPosts}
---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const cors = require("cors");
+const { agregarPosts, obtenerPosts, modificarPosts, eliminarPosts } = require("./consultas.js");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.listen(3000, console.log("¡Servidor encendido!"));
+
+// metodo get
+
+app.get("/posts", async (req, res) => {
+  try {
+    const posts = await obtenerPosts();
+    res.json(posts);
+    
+  } catch ({ code, message }) {
+    res.status(code).send(message);
+  }
+ 
+});
+
+
+// metodo post
+
+app.post("/posts", async (req, res) => {
+  try {
+    const { titulo, img, descripcion, likes } = req.body;
+    await agregarPosts(titulo, img, descripcion, likes);
+    res.send("Posts generado correctamente");
+    
+  } catch ({ code, message }) {
+    res.status(code).send(message);
+  }
+ 
+});
+
+
+//metodo put 
+
+app.put("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { titulo } = req.body;
+    await modificarPosts(id, titulo);
+    res.status(200).send("Posts modificado con éxito");
+  } catch ({ code, message }) {
+    res.status(code).send(message);
+  }
+});
+
+
+// metodo delete 
+
+app.delete("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await eliminarPosts(id);
+    res.send("Cliente eliminado con éxito");
+    
+  } catch ({ code, message }) {
+    res.status(code).send(message);
+  }
+
+});
